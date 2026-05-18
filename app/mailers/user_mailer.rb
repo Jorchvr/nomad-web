4152e0b610af961cee39efdb89258e8df7940b2d
Ramosvr (Jorchvr) <jgael_vr@hotmail.com>
2026-05-18 15:23:29 -0600

    Fix confirmation URL helper name in UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def confirmation_email(user)
     @user             = user
-    @confirmation_url = confirm_backoffice_email_url(token: user.confirmation_token)
+    @confirmation_url = backoffice_confirm_email_url(token: user.confirmation_token)
     mail(to: @user.email, subject: "Confirm your World Nomad Web account")
   end
 end
